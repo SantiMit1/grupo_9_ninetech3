2ed11e3541c6c2b1fc374f1c11aae52c4ab83899
SantiMit1 <santimiti10@gmail.com>
2022-07-21 15:44:28 -0300

    pagina de error 404 agregada

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//requires
 const express = require('express');
 const path = require("path");
 const app = express();
@@ -6,11 +7,16 @@ const mainRoutes = require("./src/routes/mainRoutes");
 const usersRoutes = require("./src/routes/usersRoutes.js");
 const productsRoutes = require("./src/routes/productsRoutes.js");
 
+//middlewares
 app.use(express.static(path.resolve(__dirname, "./public")));
 
+//rutas
 app.use("/", mainRoutes);
 app.use("/users", usersRoutes);
 app.use("/productos", productsRoutes);
+
+//404
+app.use((req, res, next) => res.status(404).render("404"));
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "./src/views"));
